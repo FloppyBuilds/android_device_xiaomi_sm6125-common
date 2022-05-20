a5fdf9f23d7ad9c846449f0c6a3323d3a95272bd
Jiakai Zhang <jiakaiz@google.com>
2022-05-20 14:40:27 +0100

    sm6125-common: Preopt SystemUI for AOSP builds.
    Bug: 232860596
    Test: See no dex2oat invocation on the first boot.
    Change-Id: Icc38efb4b702b518caa22a3abfc7c04d6ba1ac2a
    Signed-off-by: Flopster101 <nahuelgomez329@gmail.com>

--- common.mk
+++ common.mk
@@ -64,6 +64,10 @@ PRODUCT_PACKAGES += \
 # DebugFS
 PRODUCT_SET_DEBUGFS_RESTRICTIONS := true
 
+# Dexpreopt
+PRODUCT_DEXPREOPT_SPEED_APPS += \
+    SystemUI
+
 # Display
 PRODUCT_PACKAGES += \
     android.hardware.graphics.composer@2.4-service \
