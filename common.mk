8765d99fa8ec2d8504d58465af7c22c7e247333f
nitin1438 <nitin.chobhe@gmail.com>
2022-10-12 16:10:39 +0000

    sm6125-common: Drop rules for compiling qti packages.
    * Already configured in our vendor: https://github.com/pixeldust-project-caf/android_vendor_pixeldust/blob/tartufo/configs/pixeldust_caf.mk
    
    A double use in vendor and device tree would lead to a conflict:
    
    ERROR: Dex2oat failed to compile a boot image. It is likely that the boot classpath is inconsistent.
    Rebuild with ART_BOOT_IMAGE_EXTRA_ARGS="--runtime-arg -verbose:verifier" to see verification errors.
    dex2oatd F 09-16 17:46:09 1082584 1082584 class_linker.cc:1334]
    Check failed: boot_dex_file->GetLocation() != dex_file->GetLocation() (boot_dex_file->GetLocation()
    =/system/framework/telephony-ext.jar, dex_file->GetLocation()=/system/framework/telephony-ext.jar)
    
    Signed-off-by: Flopster101 <nahuelgomez329@gmail.com>

--- common.mk
+++ common.mk
@@ -239,18 +239,18 @@ PRODUCT_SOONG_NAMESPACES += \
 # Speed Profiles
 PRODUCT_SYSTEM_SERVER_COMPILER_FILTER := speed-profile
 
-# Telephony
-PRODUCT_PACKAGES += \
-    ims-ext-common \
-    ims_ext_common.xml \
-    qti-telephony-hidl-wrapper \
-    qti_telephony_hidl_wrapper.xml \
-    qti-telephony-utils \
-    qti_telephony_utils.xml \
-    telephony-ext
+# # Telephony
+# PRODUCT_PACKAGES += \
+#     ims-ext-common \
+#     ims_ext_common.xml \
+#     qti-telephony-hidl-wrapper \
+#     qti_telephony_hidl_wrapper.xml \
+#     qti-telephony-utils \
+#     qti_telephony_utils.xml \
+#     telephony-ext
 
-PRODUCT_BOOT_JARS += \
-    telephony-ext
+# PRODUCT_BOOT_JARS += \
+#     telephony-ext
 
 # Thermal
 PRODUCT_PACKAGES += \
