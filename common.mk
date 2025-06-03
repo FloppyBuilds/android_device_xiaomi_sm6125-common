709ea7ca987210a322f6f89d77c1a944e1e558f3
Flopster101 <nahuelgomez329@gmail.com>
2025-06-03 20:11:09 -0300

    sm6125-common: inherit private keys from LineageOS path
    Signed-off-by: Flopster101 <nahuelgomez329@gmail.com>

--- common.mk
+++ common.mk
@@ -285,6 +285,8 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/wifi/wpa_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant_overlay.conf \
     $(LOCAL_PATH)/configs/wifi/WCNSS_qcom_cfg.ini:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/WCNSS_qcom_cfg.ini
 
+# Private keys
+-include vendor/lineage-priv/keys/keys.mk
 
 # Inherit the proprietary files
 $(call inherit-product, vendor/xiaomi/sm6125-common/sm6125-common-vendor.mk)
