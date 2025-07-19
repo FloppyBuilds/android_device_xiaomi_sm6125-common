2b4b2ca9449df50a6e7027f60b2f86eb7ebd30ee
Flopster101 <nahuelgomez329@gmail.com>
2025-07-18 22:21:35 -0300

    sm6125-common: Initialize Pixelage
    Signed-off-by: Flopster101 <nahuelgomez329@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -40,7 +40,7 @@ DEVICE_MANIFEST_FILE += $(COMMON_PATH)/manifest.xml
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
     hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
     hardware/xiaomi/vintf/xiaomi_framework_compatibility_matrix.xml \
-    vendor/aosp/config/device_framework_matrix.xml
+    vendor/pixelage/config/device_framework_matrix.xml
 
 # Kernel
 BOARD_BOOTIMG_HEADER_VERSION := 2
