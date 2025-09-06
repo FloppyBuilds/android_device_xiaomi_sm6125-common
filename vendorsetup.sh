aef6851baf510c1fd40d4406f1471415f1563180
Flopster101 <nahuelgomez329@gmail.com>
2025-09-05 22:50:19 -0300

    sm6125-common: vendorsetup: Apply patch to force 4G icon
    This ROM's CarrierConfig overlay way of doing this is broken.
    
    Signed-off-by: Flopster101 <nahuelgomez329@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,3 +1,5 @@
+REPO_DIR="$(realpath device/xiaomi/sm6125-common)"
+
 if [ -f "hardware/lineage/interfaces/health/aidl/default/FastCharge.cpp" ]; then
   echo "Reverting IFastCharge HAL changes..."
 
@@ -17,4 +19,15 @@ if ! grep -q "disabled" "bootable/deprecated-ota/applypatch/vendor_flash_recover
   sed -i '/^service vendor_flash_recovery \/vendor\/bin\/install-recovery\.sh$/a\    disabled' vendor_flash_recovery.rc
   
   popd > /dev/null
+fi
+
+if ! grep -q "config.show4gForLte = true" "frameworks/base/packages/SettingsLib/src/com/android/settingslib/mobile/MobileMappings.java"; then
+  echo "Applying 4G icon force patch..."
+  
+  pushd "frameworks/base" > /dev/null
+  
+  git apply "$REPO_DIR/patches/0001-Force-4G-icon-on-status-bar-for-all-carriers.patch"
+  
+  popd > /dev/null
+  echo "4G icon patch applied successfully."
 fi
