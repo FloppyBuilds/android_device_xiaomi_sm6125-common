8e369b2dfbd379567f1718c18919f5952c037204
Flopster101 <nahuelgomez329@gmail.com>
2025-09-07 14:01:09 -0300

    Revert "sm6125-common: Switch to AIDL camera HAL"
    Not working in Axion.
    
    This reverts commit ba5fd8e6c63f58ae857999de22468b43c217776e.

--- common.mk
+++ common.mk
@@ -43,7 +43,8 @@ PRODUCT_PACKAGES += \
 
 # Camera
 PRODUCT_PACKAGES += \
-    android.hardware.camera.provider-service_32.lineage
+    android.hardware.camera.provider@2.4-impl \
+    android.hardware.camera.provider@2.4-service
 
 # Charger
 PRODUCT_PACKAGES += \
