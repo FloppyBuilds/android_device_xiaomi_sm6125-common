687ca9f4d8b805a68f58b83e89cc2419fef90648
Flopster101 <nahuelgomez329@gmail.com>
2025-12-22 23:44:13 +0000

    sm6125-common: Explicitly set livedisplay_sdm enable_pa to false
    Probably not needed, but I was having some issues earlier building
    crDroid 12.5.
    
    Change-Id: I4d1b901e4822225704159338b7d52db38871cf85
    Signed-off-by: Flopster101 <nahuelgomez329@gmail.com>

--- common.mk
+++ common.mk
@@ -125,6 +125,7 @@ PRODUCT_PACKAGES += \
     vendor.lineage.livedisplay-service.sdm
 
 $(call soong_config_set,livedisplay_sdm,enable_dm,false)
+$(call soong_config_set,livedisplay_sdm,enable_pa,false)
 
 # Media configs
 PRODUCT_PACKAGES += \
